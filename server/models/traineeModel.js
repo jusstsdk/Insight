b0db7003b814182256cd62ddce84079ce761a6f4
YousefAllam221B <yousef.allam221b@gmail.com>
2022-10-27 18:57:19 +0200

    removing unwanted comments

--- server/models/traineeModel.js
+++ server/models/traineeModel.js
@@ -2,14 +2,6 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-// const courseTraineeSchema = new Schema({
-// 	courseID: {
-// 		type: Schema.ObjectId, //references courseModel
-// 		required: true,
-// 	},
-// 	exercisesSolved: [reportSchema],
-// });
-
 const questionSchema = new Schema({
 	question: String,
 	grade: Number,
@@ -20,18 +12,6 @@ const exerciseSchema = new Schema({
 	maxGrade: Number, // could be a calculated attribute from total number of questions
 	recievedGrade: Number, // could be a calculated attribute from sum of grades
 });
-
-// exercises is an array of exercise
-// exercise is an array of questions
-// questions is an object with question: String and grade: Number
-// exercises: [exercise : [{
-// 	question: String,
-// 	grade: Number
-// }]]
-// const exerciseTraineeSchema = new Schema({
-// 	exerciseID: [], //refrences exerciseModel
-// 	grade: Number,
-// });
 
 const traineeSchema = new Schema(
 	{
