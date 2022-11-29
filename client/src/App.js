bc611a98d12535dbe3b541cee692eba5442c3f19
YousefAllam221B <yousef.allam221b@gmail.com>
2022-11-29 23:20:52 +0200

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -5,32 +5,34 @@ function App() {
 	const { token } = useSelector((state) => state.counter);
 	const dispatch = useDispatch();
 	const loginFunction = async () => {
-		const config = {
-			method: "post",
-			url: "http://localhost:4000/api/users/login",
-			headers: {},
-			data: {
-				username: "admin62",
-				password: "pass1",
-			},
-		};
+		let token = localStorage.getItem("token");
+		if (token == null || token == "") {
+			console.log("NOT THERE");
+			const config = {
+				method: "post",
+				url: "http://localhost:4000/api/users/login",
+				headers: {},
+				data: {
+					username: "admin62",
+					password: "pass1",
+				},
+			};
 
-		let response = await axios(config);
-		// console.log(response.data["x-auth-token"]);
-		localStorage.setItem("token", JSON.stringify(response.data["x-auth-token"]));
-		// dispatch(setToken(response.data.token));
-	};
-	const getToken = async () => {
-		const token = JSON.parse(localStorage.getItem("token"));
+			let response = await axios(config);
+			localStorage.setItem("token", JSON.stringify(response.data["x-auth-token"]));
+			token = response.data["x-auth-token"];
+		} else {
+			console.log("THERE");
+			console.log(token);
+		}
 
 		dispatch(setToken(token));
-		console.log(token);
+		// dispatch(setToken(response.data.token));
 	};
 	return (
 		<div className="App">
 			<h1>The token is {token}</h1>
 			<button onClick={() => loginFunction()}> Login </button>
-			<button onClick={() => getToken()}> Get Token </button>
 		</div>
 	);
 }
