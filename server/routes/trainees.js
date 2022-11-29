ef34c0ee26683b6bd3d82bee95a705e044cee1ea
amna97531 <83308490+amna97531@users.noreply.github.com>
2022-11-29 18:38:13 +0400

    wip

--- server/routes/trainees.js
+++ server/routes/trainees.js
@@ -1,26 +1,25 @@
 const express = require("express");
 
 const {
-  createTrainee,
-  getTrainees,
-  getTrainee,
-  updateTrainee,
-  deleteTrainee,
-  subscribeTraineeToCourse,
-  requestRefund,
+	createTrainee,
+	getTrainees,
+	getTrainee,
+	updateTrainee,
+	deleteTrainee,
+	subscribeTraineeToCourse,
+	requestRefund,
 } = require("../controllers/traineeController");
 
 const router = express.Router();
 
-// POST a new administrator
 router.post("/", createTrainee);
-// POST a new administrator
+
 router.get("/", getTrainees);
-// POST a new administrator
+
 router.get("/:id", getTrainee);
-// POST a new administrator
+
 router.put("/:id", updateTrainee);
-// POST a new administrator
+
 router.delete("/:id", deleteTrainee);
 
 router.post("/:traineeId/requestRefund/courses/:courseId", requestRefund);
