277e485594efc4e0f48a40f84be3a0d6ca609d10
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2022-12-01 20:16:58 +0200

    redirect "/" to correct page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
 import Login from "./components/Login";
 import Home from "./pages/Home";
 import { Link, Route, Routes, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { setToken, setType } from "./redux/userSlice";
 import Protected from "./components/Protected";
+import { Navigate } from "react-router-dom";
 
 function App() {
-	const dispatch = useDispatch();
-	
-	useEffect(() => {
-		const storedToken = localStorage.getItem("token");
-		const storedUsertype = localStorage.getItem("userType");
-		if (!(storedToken === null) && !(storedToken === "")) {
-			dispatch(setToken(JSON.parse(storedToken)));
-			dispatch(setType(JSON.parse(storedUsertype)));
-		}
-	}, []);
-
 	return (
 		<div className="App">
 			<Routes>
+				<Route
+					path="/"
+					element={
+						<Protected>
+							<Navigate to="/home" replace/>
+						</Protected>
+					}
+				/>
 				<Route path="/login" element={<Login />} />
 				<Route
 					path="/home"
--- client/src/components/Protected.js
+++ client/src/components/Protected.js
@@ -1,7 +1,24 @@
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setToken, setType } from "../redux/userSlice";
+
 const Protected = ({ children }) => {
 	const userType = useSelector((state) => state.userReducer.type);
+
+    const dispatch = useDispatch();
+
+	useEffect(() => {
+		const storedToken = localStorage.getItem("token");
+		const storedUsertype = localStorage.getItem("userType");
+		if (!(storedToken === null) && !(storedToken === "")) {
+			dispatch(setToken(JSON.parse(storedToken)));
+			dispatch(setType(JSON.parse(storedUsertype)));
+		}
+	}, []);
+
+    console.log(userType);
 
 	if (!userType) {
 		return <Navigate to="/login" replace />;
