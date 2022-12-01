e891bd925d95710280c366f771204d159dbfbcaf
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2022-12-01 20:24:10 +0200

    better logic

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,23 @@ import Home from "./pages/Home";
 import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import Protected from "./components/Protected";
 import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setToken, setType } from "./redux/userSlice";
 
 function App() {
+	const dispatch = useDispatch();
+
+	// check if user local storage contains creds
+	useEffect(() => {
+		const storedToken = localStorage.getItem("token");
+		const storedUsertype = localStorage.getItem("userType");
+		if (!(storedToken === null) && !(storedToken === "")) {
+			dispatch(setToken(JSON.parse(storedToken)));
+			dispatch(setType(JSON.parse(storedUsertype)));
+		}
+	}, []);
+
 	return (
 		<div className="App">
 			<Routes>
--- client/src/components/Protected.js
+++ client/src/components/Protected.js
@@ -1,24 +1,8 @@
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { setToken, setType } from "../redux/userSlice";
 
 const Protected = ({ children }) => {
-	const userType = useSelector((state) => state.userReducer.type);
-
-    const dispatch = useDispatch();
-
-	useEffect(() => {
-		const storedToken = localStorage.getItem("token");
-		const storedUsertype = localStorage.getItem("userType");
-		if (!(storedToken === null) && !(storedToken === "")) {
-			dispatch(setToken(JSON.parse(storedToken)));
-			dispatch(setType(JSON.parse(storedUsertype)));
-		}
-	}, []);
-
-    console.log(userType);
+    	const userType = useSelector((state) => state.userReducer.type);
 
 	if (!userType) {
 		return <Navigate to="/login" replace />;
