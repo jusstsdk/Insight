e77dd79540e870109cc20ef2b9e2ba2d108da2b4
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2022-12-11 18:26:42 +0200

    removed login css

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./css/index.css";
-import "./css/login.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
