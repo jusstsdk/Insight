1dd7507ff2c782b538ebcae01f843e18285f46ea
YousefAllam221B <yousef.allam221b@gmail.com>
2022-12-18 21:27:23 +0200

    Clean up

--- client/src/redux/createCourseSlice.js
+++ client/src/redux/createCourseSlice.js
@@ -96,65 +96,11 @@ export const createCourseSlice = createSlice({
 			state.subtitles[action.payload.subtitleKey].exercises[action.payload.exerciseKey].questions =
 				action.payload.newQuestions;
 		},
-		// Not sure
-		// addSubtitle: (state, action) => {
-		// 	let newSubtitle = {
-		// 		title: state.subtitleTitle,
-		// 		hours: state.subtitleHours,
-		// 		videos: state.subtitleVideos,
-		// 		exercises: state.subtitleExercises,
-		// 	};
-		// 	state.subtitleTitle = "";
-		// 	state.subtitleHours = 0;
-		// 	state.subtitleVideos = [];
-		// 	state.subtitleExercises = [];
-		// 	state.subtitles = [...state.subtitles, newSubtitle];
-		// },
-
-		setSubtitleTitle: (state, action) => {
-			state.subtitleTitle = action.payload;
-		},
-		setSubtitleHours: (state, action) => {
-			state.subtitleHours = action.payload;
-		},
-		addExerciseToSubtitleExercises: (state, action) => {
-			let newExercise = {
-				title: state.exerciseTitle,
-				questions: state.exerciseQuestions,
-			};
-			state.exerciseTitle = "";
-			state.exerciseQuestions = [];
-			state.subtitleExercises = [...state.subtitleExercises, newExercise];
-		},
-		removeExerciseFromSubtitleExercises: (state, action) => {
-			state.subtitleExercises = state.subtitleExercises.filter(
-				(exercise, i) => i !== action.payload
-			);
-		},
-		// addVideoToSubtitle: (state, action) => {
-		// 	state.subtitleVideos = [...state.subtitleVideos, action.payload];
-		// },
-		removeSubtitleVideos: (state, action) => {
-			state.subtitleVideos = state.subtitleVideos.filter((video, i) => i !== action.payload);
-		},
-		setExerciseTitle: (state, action) => {
-			state.exerciseTitle = action.payload;
-		},
-		addToExerciseQuestions: (state, action) => {
-			state.exerciseQuestions = [...state.exerciseQuestions, action.payload];
-		},
-		removeExerciseQuestions: (state, action) => {
-			state.exerciseQuestions = state.exerciseQuestions.filter(
-				(question, i) => i !== action.payload
-			);
-		},
 		clearCreateCourse: (state, action) => {
 			state.examTitle = "";
 			state.examQuestions = [];
 			state.subtitles = [];
 		},
-
-		// // logout: (state) => {},
 	},
 });
 
@@ -179,14 +125,7 @@ export const {
 	addQuestionToExercise,
 	editQuestionOfExercise,
 	removeQuestionFromExercise,
-
-	removeSubtitleVideos,
-	setExerciseTitle,
-	addToExerciseQuestions,
-	removeExerciseQuestions,
-	addExerciseToSubtitleExercises,
 	removeSubtitle,
-	removeExerciseFromSubtitleExercises,
 	clearCreateCourse,
 } = createCourseSlice.actions;
 
