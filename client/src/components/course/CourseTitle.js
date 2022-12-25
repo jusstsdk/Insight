4ecd59daf4fa65525271626b8cc4c4d0eb7ee70e
Mahmoud-Elshirbeny <mahmoud.o.elshirbeny@gmail.com>
2022-12-26 01:59:39 +0200

    cleaned up coursePage

--- client/src/components/course/CourseTitle.js
+++ client/src/components/course/CourseTitle.js
@@ -3,19 +3,28 @@ import { Badge, Col, Row } from "react-bootstrap";
 function CourseTitle(props) {
 	const course = props.course;
 	return (
-		<>
-			<h2 style={{ display: "inline-block" }}>{course.title} </h2>
-
-			{course.subjects.map((eachSubject) => {
+		<div className="page-header" key="titleDiv">
+			<h1 style={{ display: "inline-block" }} key="titleHeader">
+				{course.title + " "}
+			</h1>
+			&nbsp;&nbsp;&nbsp;&nbsp;
+			{course.subjects.map((eachSubject, i) => {
 				return (
-					<Badge key={eachSubject} bg="primary" className="lead">
-						{eachSubject}
-					</Badge>
+					<>
+						<Badge
+							key={"Badge of Subject: " + i + eachSubject}
+							bg="primary"
+							className="lead"
+						>
+							{eachSubject}
+						</Badge>
+						&nbsp;
+					</>
 				);
 			})}
-
 			<br />
-		</>
+			<br />
+		</div>
 	);
 }
 
