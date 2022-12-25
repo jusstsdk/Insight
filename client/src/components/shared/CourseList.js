0962393a590b814acadaa3067b94d90c28eae51c
wagdy <ahmed.wagdi.2001@gmail.com>
2022-12-25 02:49:41 +0200

    done(added to search and myCourses)

--- client/src/components/shared/CourseList.js
+++ client/src/components/shared/CourseList.js
@@ -1,7 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import CourseCard from "../CourseCard";
+import { useState, useMemo, useEffect } from "react";
+import Pagination from "./pagination/Pagination";
+import "./pagination/style.scss";
 
+let pageSize = 2;
 export default function CourseList({ courses }) {
-	return courses.map((course, i) => (
-		<CourseCard key={"course" + i} course={course} />
-	));
+	const [currentPage, setCurrentPage] = useState(1);
+	let firstPageIndex = (currentPage - 1) * pageSize;
+	let lastPageIndex = firstPageIndex + pageSize;
+	let currentCourses = courses.slice(firstPageIndex, lastPageIndex);
+	// currentCourses = useMemo(() => {
+	// 	firstPageIndex = (currentPage - 1) * pageSize;
+	// 	lastPageIndex = firstPageIndex + pageSize;
+	// 	console.log(courses);
+	// 	return courses.slice(firstPageIndex, lastPageIndex);
+	// }, [currentPage]);
+
+	
+	return (
+		
+			<>
+				{currentCourses.map((course, i) => (
+					<CourseCard key={"course" + i} course={course} />
+				))}
+				<Pagination
+					className="pagination-bar"
+					currentPage={currentPage}
+					totalCount={courses.length}
+					pageSize={pageSize}
+					onPageChange={(page) => setCurrentPage(page)}
+				/>
+			</>
+		
+	);
 }
