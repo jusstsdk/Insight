39e9097ef2824fa4b40c22c422f21c6d36a69926
YousefAllam221B <yousef.allam221b@gmail.com>
2022-12-25 19:38:51 +0200

    removed continueCourse from CourseSubtitles

--- client/src/components/course/CourseSubtitlesList.js
+++ client/src/components/course/CourseSubtitlesList.js
@@ -1,25 +1,13 @@
 import { Button, ListGroup, Table, Row, Col } from "react-bootstrap";
 
 import React from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-
 function CourseSubtitlesList({ course }) {
-	const navigate = useNavigate();
-	const userCourse = useSelector(
-		(state) => state.userReducer.user.courses
-	).filter((course) => {
-		return course._id == course._id;
-	})[0];
-
 	return (
 		<>
 			<ListGroup>
-				{userCourse.subtitles.map((subtitle, subtitle_index) => {
+				{course.subtitles.map((subtitle, subtitle_index) => {
 					return (
-						<ListGroup.Item
-							key={`Subtitles_tr_${course._id}_${subtitle_index}`}
-						>
+						<ListGroup.Item key={`Subtitles_tr_${course._id}_${subtitle_index}`}>
 							<Row>
 								<Col>#{subtitle_index + 1}</Col>
 								<Col>{subtitle.title ? subtitle.title : "No Title."}</Col>
