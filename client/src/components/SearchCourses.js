a0c80a96850ff960feea4c055ca6c43e774513f6
YousefAllam221B <yousef.allam221b@gmail.com>
2022-12-31 02:42:32 +0200

    New Search and filterBar

--- client/src/components/SearchCourses.js
+++ client/src/components/SearchCourses.js
@@ -1,4 +1,4 @@
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row, Collapse } from "react-bootstrap";
 import { useEffect, useRef, useState } from "react";
 import API from "../functions/api";
 import { useSelector } from "react-redux";
@@ -7,7 +7,8 @@ import { Multiselect } from "multiselect-react-dropdown";
 import { Drawer, List, Toolbar } from "@mui/material";
 import { Box } from "@mui/system";
 import { Rating } from "react-simple-star-rating";
-import { BsSearch } from "react-icons/bs";
+import { BsFilter, BsSearch } from "react-icons/bs";
+import { AiOutlineClose } from "react-icons/ai";
 const drawerWidth = "25%";
 
 export default function SearchCourses({ setCourses, searchInInstructorCourses, hideSearch }) {
@@ -20,20 +21,17 @@ export default function SearchCourses({ setCourses, searchInInstructorCourses, h
 	const user = useSelector((state) => state.userReducer.user);
 
 	const [sort, setSort] = useState(false);
+	const [open, setOpen] = useState(false);
 
 	function comparePopularity(a, b) {
 		if (a.enrolledTrainees.length > b.enrolledTrainees.length) return -1;
 		if (a.enrolledTrainees.length < b.enrolledTrainees.length) return 1;
 		return 0;
 	}
-	async function handleSubmit(e) {
-		e.preventDefault();
-		await getCourses();
-	}
 
 	useEffect(() => {
 		getCourses();
-	}, [user.country, sort]);
+	}, [user.country, sort, ratingFilter, subjectFilter]);
 
 	async function getCourses() {
 		let searchParams = {};
@@ -64,110 +62,84 @@ export default function SearchCourses({ setCourses, searchInInstructorCourses, h
 		if (sort) courses.sort(comparePopularity);
 		setCourses(courses);
 	}
-	const mainNavbar = document.getElementById("main-navbar");
 
 	// Displays the Drawer Content based on props.subtitles
-	const drawer = (
-		<div className="">
-			{/* Filler to avoid Navbar */}
-			<Toolbar
-				sx={{
-					marginTop: {
-						sm: `${mainNavbar ? mainNavbar.offsetHeight : ""}px`,
-					},
-				}}
-			/>
-			<Container id="searchDrawer" className="my-auto">
-				{!hideSearch && (
-					<>
-						<h3 className="mb-3">Filters</h3>
-						<Form.Group className="mb-3" controlId="formSubjectFilter">
-							<Multiselect
-								id="singleSelectSubjects"
-								options={SUBJECTS}
-								selectedValues={subjectFilter ? [subjectFilter] : []}
-								onSelect={(_, selectedItem) => {
-									setSubjectFilter(selectedItem);
-								}}
-								onRemove={() => {
-									setSubjectFilter("");
-								}}
-								isObject={false}
-								placeholder="Select Subject Filter"
-								closeOnSelect={true}
-								showArrow={true}
-								avoidHighlightFirstOption={true}
-								hidePlaceholder={true}
+	const filterBar = (
+		<>
+			{!hideSearch && (
+				<div className="d-flex  ">
+					<Col sm={5} className=" align-items-center">
+						<Multiselect
+							id="singleSelectSubjects"
+							options={SUBJECTS}
+							selectedValues={subjectFilter ? [subjectFilter] : []}
+							onSelect={(_, selectedItem) => {
+								setSubjectFilter(selectedItem);
+							}}
+							onRemove={() => {
+								setSubjectFilter("");
+							}}
+							isObject={false}
+							placeholder="Select Subject Filter"
+							closeOnSelect={true}
+							showArrow={true}
+							avoidHighlightFirstOption={true}
+							hidePlaceholder={true}
+						/>
+					</Col>
+
+					<Col
+						sm={3}
+						className={`d-flex justify-content-around align-items-center ${
+							!searchInInstructorCourses ? "mx-auto" : "ms-2"
+						}`}>
+						<Form.Label className="fitWidth my-auto">Price</Form.Label>
+						<Col className="me-1" sm={4}>
+							<Form.Control
+								ref={minPriceFilter}
+								onChange={async () => await getCourses()}
+								type="number"
+								placeholder="Min"
 							/>
-						</Form.Group>
+						</Col>
+						<Col className="" sm={4}>
+							<Form.Control
+								ref={maxPriceFilter}
+								onChange={async () => await getCourses()}
+								type="number"
+								placeholder="Max"
+							/>
+						</Col>
+					</Col>
 
-						<Form.Group className="my-3 d-flex" controlId="formPriceFilter">
-							<Form.Label className="fitWidth my-auto me-1">Price</Form.Label>
-							<Col className="p-0 me-1 ms-auto" sm={4}>
-								<Form.Control ref={minPriceFilter} type="number" placeholder="Min" />
-							</Col>
-							<Col className="p-0 me-1" sm={4}>
-								<Form.Control ref={maxPriceFilter} type="number" placeholder="Max" />
-							</Col>
-						</Form.Group>
-
-						{!searchInInstructorCourses && (
-							<Form.Group
-								className="mb-3 d-flex  justify-content-between "
-								controlId="formRatingFilter">
-								<Form.Label className="me-3 my-auto">Min Rating</Form.Label>
-								<div className="fitWidth">
-									<Rating
-										allowFraction="true"
-										onClick={(rating) => setRatingFilter(rating)}
-										/* Available Props */
-									/>
-								</div>
-							</Form.Group>
-						)}
-						<Form.Group className="d-flex justify-content-between">
-							<Col sm={8}>
-								<Form.Check
-									type="checkbox"
-									id={"default-checkbox"}
-									label="Sort by popularity"
-									className="fitWidth"
-									onChange={() => setSort(!sort)}
+					{!searchInInstructorCourses && (
+						<Col
+							sm={3}
+							className="d-flex  justify-content-between align-items-center "
+							controlId="formRatingFilter">
+							<Form.Label className="fitWidth my-auto">Min Rating</Form.Label>
+							<div className="fitWidth me-2">
+								<Rating
+									allowFraction="true"
+									onClick={(rating) => setRatingFilter(rating)}
+									size={28}
+									initialValue={ratingFilter}
 								/>
-							</Col>
-
-							<Button onClick={async () => await getCourses()}>Filter</Button>
-						</Form.Group>
-					</>
-				)}
-			</Container>
-		</div>
+							</div>
+							<Button onClick={() => setRatingFilter(0)} className="clearRatingButton">
+								<AiOutlineClose color={"#dc3545"} />
+							</Button>
+						</Col>
+					)}
+				</div>
+			)}
+		</>
 	);
 
 	return (
 		<>
-			<Box
-				// key={`course_${Course._id}_drawer_box`}
-				className="drawerZ-index"
-				component="nav"
-				sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-				aria-label="mailbox folders">
-				{/* Normal Screen Drawer */}
-				<Drawer
-					// key={`course_${Course._id}_normal_screen_drawer`}
-					variant="permanent"
-					sx={{
-						display: { xs: "none", sm: "block" },
-						"& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
-					}}
-					open>
-					{drawer}
-				</Drawer>
-			</Box>
 			<Row className="mb-3 align-items-center">
-				<Form.Label className="fitWidth my-auto">Search Term</Form.Label>
-
-				<Col sm={9}>
+				<Col sm={8}>
 					<Form.Control
 						ref={searchQuery}
 						type="search"
@@ -179,6 +151,26 @@ export default function SearchCourses({ setCourses, searchInInstructorCourses, h
 						<BsSearch />
 					</Button>
 				</Col>
+
+				<Col sm={2}>
+					<Form.Check
+						type="checkbox"
+						id={"default-checkbox"}
+						label="Sort by popularity"
+						className="fitWidth"
+						onChange={() => setSort(!sort)}
+					/>
+				</Col>
+				<Col sm={1} className="d-flex justify-content-end">
+					<Button onClick={() => setOpen(!open)} aria-controls="filterBar" aria-expanded={open}>
+						<BsFilter />
+					</Button>
+				</Col>
+				<Collapse in={open}>
+					<div id="filterBar" className="mt-3">
+						{filterBar}
+					</div>
+				</Collapse>
 			</Row>
 		</>
 	);
