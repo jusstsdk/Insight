6b6683031f7ea39f8651c9c93f4fdbad6fb61ba4
YousefAllam221B <yousef.allam221b@gmail.com>
2023-01-02 00:04:21 +0200

    Signups

--- client/src/pages/admin/CreateCorporateTrainee.js
+++ client/src/pages/admin/CreateCorporateTrainee.js
@@ -2,9 +2,15 @@ import { useRef, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import axios from "axios";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Col, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { addNotification } from "../../redux/notificationsSlice";
 
 function CreateCorporateTrainee() {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
 	const token = useSelector((state) => state.userReducer.token);
 	const Username = useRef();
 	const Password = useRef();
@@ -23,44 +29,63 @@ function CreateCorporateTrainee() {
 			},
 		};
 		try {
-			let response = await axios(config);
+			await axios(config);
+			dispatch(
+				addNotification({
+					title: "Corporate Trainee Created SuccessFully",
+					info: "Send the entered Username and Password to them to Login",
+					color: "success",
+				})
+			);
+			navigate("/");
 		} catch (err) {
 			console.log(err);
+			dispatch(
+				addNotification({
+					title: "Something Went Wrong",
+					info: "Try again another time please",
+					color: "error",
+				})
+			);
 		}
 	}
 	return (
 		<div>
-			<h1> create a Corporate Trainee </h1>
-			<Form onSubmit={handleCreateCorporateTrainee}>
-				<Form.Group className="mb-3">
-					<Form.Label> Username </Form.Label>
-					<Form.Control
-						ref={Username}
-						placeholder="Enter Username"
-						required
-					/>
-				</Form.Group>
-				<Form.Group className="mb-3">
-					<Form.Label>Password</Form.Label>
-					<Form.Control
-						ref={Password}
-						type="password"
-						placeholder="Password"
-						required
-					/>
-				</Form.Group>
-				<Form.Group className="mb-3">
-					<Form.Label>Corporate Name</Form.Label>
-					<Form.Control
-						ref={corporate}
-						placeholder="Corporate Name"
-						required
-					/>
-				</Form.Group>
-
-				<Button variant="primary" type="submit">
-					Add Corporate Trainee
-				</Button>
+			<h1 className="fst-italic mx-auto fitWidth">Create a Corporate Trainee</h1>
+			<Form
+				className="d-flex flex-column justify-content-center"
+				onSubmit={handleCreateCorporateTrainee}>
+				<Row className=" justify-content-center">
+					<Col sm={4}>
+						<Form.Group className="mb-3">
+							<Form.Label className="fst-italic">Username</Form.Label>
+							<Form.Control ref={Username} placeholder="Enter Username" required />
+						</Form.Group>
+					</Col>
+				</Row>
+				<Row className=" justify-content-center">
+					<Col sm={4}>
+						<Form.Group className="mb-3">
+							<Form.Label className="fst-italic">Password</Form.Label>
+							<Form.Control ref={Password} type="password" placeholder="Password" required />
+						</Form.Group>
+					</Col>
+				</Row>
+				<Row className=" justify-content-center">
+					<Col sm={4}>
+						<Form.Group className="mb-3">
+							<Form.Label className="fst-italic">Corporate Name</Form.Label>
+							<Form.Control ref={corporate} placeholder="Corporate Name" required />
+						</Form.Group>
+					</Col>
+				</Row>
+				<Row className="justify-content-center">
+					<Col sm={3} className="d-flex justify-content-center">
+						<Button className="w-100" variant="primary" type="submit">
+							Add Corporate Trainee
+						</Button>
+					</Col>
+				</Row>
 			</Form>
 		</div>
 	);
