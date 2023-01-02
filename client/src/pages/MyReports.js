b31b19b6fc813fcf57861904d6350cd8a6848e3b
Mahmoud-Elshirbeny <mahmoud.o.elshirbeny@gmail.com>
2023-01-02 02:36:30 +0200

    trash stuff for instructor

--- client/src/pages/MyReports.js
+++ client/src/pages/MyReports.js
@@ -19,14 +19,19 @@ const MyReports = () => {
 
 	return (
 		<>
+			<h1>Reports</h1>
+			<br />
 			{myCoursesReports.map((courseReports) => (
 				<Card className="mb-3" key={courseReports._id}>
 					<Card.Header className="d-flex justify-content-between align-items-center">
 						<Col sm={9}>
-							<h3>{courseReports.title}</h3>
+							Course:<h3> {courseReports.title}</h3>
 						</Col>
 						<Col sm={1}>
-							<h3 className="ms-auto me-2 fitWidth">{courseReports.reports.length}</h3>
+							# of Reports:
+							<h3 className="ms-auto me-2 fitWidth">
+								{courseReports.reports.length}
+							</h3>
 						</Col>
 					</Card.Header>
 					<Card.Body className="d-flex flex-wrap">
