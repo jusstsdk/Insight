c74c2b4a7480dd3fc3d83812faaf13b7b0492e54
YousefAllam221B <yousef.allam221b@gmail.com>
2023-01-02 05:20:36 +0200

    ExercisesList and Url Parser

--- client/src/components/course/CourseSubtitlesList.js
+++ client/src/components/course/CourseSubtitlesList.js
@@ -1,21 +1,49 @@
-import { Button, ListGroup, Table, Row, Col } from "react-bootstrap";
+import { Button, ListGroup, Table, Row, Col, Modal } from "react-bootstrap";
 import { MdTimer } from "react-icons/md";
-import React from "react";
+import React, { useState } from "react";
 function CourseSubtitlesList({ course }) {
+	const [show, setShow] = useState(false);
+	const [SubtitleTitle, setSubtitleTitle] = useState("");
+	const [Exercises, setExercises] = useState([]);
+
+	const handleClose = () => {
+		setSubtitleTitle("");
+		setExercises([]);
+		setShow(false);
+	};
+	const handleShow = (title, exercises) => {
+		setSubtitleTitle(title);
+		setExercises(exercises);
+		setShow(true);
+	};
+
 	return (
 		<>
 			<h3 className="fst-italic">Subtitles</h3>
-			<Col sm={8}>
+			<Col>
 				<ListGroup variant="flush">
 					{course.subtitles.map((subtitle, subtitle_index) => {
 						return (
-							<ListGroup.Item className="ps-0" key={`Subtitles_tr_${course._id}_${subtitle_index}`}>
+							<ListGroup.Item
+								className="ps-0 align-items-center"
+								key={`Subtitles_tr_${course._id}_${subtitle_index}`}>
 								<Row>
 									<Col sm={1}>#{subtitle_index + 1}</Col>
-									<Col sm={8}>{subtitle.title ? subtitle.title : "No Title."}</Col>
-									<Col sm={3} className="d-flex align-items-center">
+									<Col sm={7}>{subtitle.title ? subtitle.title : "No Title."}</Col>
+									<Col sm={2} className="d-flex align-items-center">
 										<MdTimer className="me-1" />
 										<p className="fitWidth lead mb-0">{Math.ceil(subtitle.seconds / 60)} Minutes</p>
+									</Col>
+									<Col sm={2} className="d-flex justify-content-end">
+										{subtitle.exercises.length !== 0 ? (
+											<Button
+												className="fitWidth"
+												onClick={() => handleShow(subtitle.title, subtitle.exercises)}>
+												View Exercises
+											</Button>
+										) : (
+											<h6 className="text-muted">No Exercises</h6>
+										)}
 									</Col>
 								</Row>
 							</ListGroup.Item>
@@ -23,6 +51,29 @@ function CourseSubtitlesList({ course }) {
 					})}
 				</ListGroup>
 			</Col>
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Subtitle: {SubtitleTitle}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<ListGroup variant="flush">
+						{Exercises.map((exercise, exercise_index) => {
+							return (
+								<ListGroup.Item
+									className="ps-0"
+									key={`Subtitles_tr_${course._id}_${exercise_index}`}>
+									{exercise_index + 1}. {exercise.title}
+								</ListGroup.Item>
+							);
+						})}
+					</ListGroup>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Close
+					</Button>
+				</Modal.Footer>
+			</Modal>
 		</>
 	);
 }
